1140ee75b355700695002e1d90125702023db706
Kevin <kevinsuyadi666@gmail.com>
2025-07-17 15:54:07 +0700

    add contact-info

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -31,7 +31,10 @@ function Contact() {
       <h2 className="contact-title">Contact Me</h2>
       <div className="contact-container">
         <div className="contact-info">
-          {/* ...info... */}
+          <p><strong>Nama :</strong> Kevin Suyadi Ritonga</p>
+          <p><strong>Email :</strong> <a href="mailto:kevinsuyadi2017@gmail.com">kevinsuyadi2017@gmail.com</a></p>
+          <p><strong>No.Telp/ Whatsapp :</strong> <a href="https://wa.link/uve1tb">+62 856-9329-5680</a></p>
+          <p><strong>Alamat :</strong> Jl. Kenanga 1 No. 18 Blok D 21, Wanasari, Cibitung, Jawa Barat, Indonesia</p>
         </div>
         <form className="contact-form" onSubmit={handleSubmit}>
           <h3>Contact Me</h3>
