aa1a16e4280be6a16a4d577512d5055e009354b5
Kevin <kevinsuyadi666@gmail.com>
2025-07-17 17:31:59 +0700

    add icon on Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './css/Footer.css';
+import { FaYoutube, FaGithub, FaInstagram, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; 2025 Portfolio. All Rights Reserved.</p>
+
+      <div className="footer-info">
+        <p><FaEnvelope /> kevinsuyadi2017@gmail.com</p>
+        <p><FaMapMarkerAlt /> Cibitung, Jawa Barat, Indonesia</p>
+      </div>
+
       <div className="social-links">
-        <a href="https://www.youtube.com/@kevindoots" target="_blank" rel="noopener noreferrer">Youtube</a>
-        <a href="https://github.com/Vinez666" target="_blank" rel="noopener noreferrer">Github</a>
-        <a href="https://www.instagram.com/kepin.sr/" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://www.youtube.com/@kevindoots" target="_blank" rel="noopener noreferrer">
+          <FaYoutube /> Youtube
+        </a>
+        <a href="https://github.com/Vinez666" target="_blank" rel="noopener noreferrer">
+          <FaGithub /> Github
+        </a>
+        <a href="https://www.instagram.com/kepin.sr/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram /> Instagram
+        </a>
       </div>
     </footer>
   );
