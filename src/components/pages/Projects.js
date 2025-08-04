11c5aca7597060d0413ef8c66b1566b1dd057847
Kevin <kevinsuyadi666@gmail.com>
2025-08-05 01:44:29 +0700

    card projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,9 +5,10 @@ function Projects() {
   const projects = [
     {
       title: "Company Profile",
-      description: "I created this company profile project to promote it to a certain company.",
+      description: "I created and implemented this company profile website project to offer to a specific company that needed a simple and easy-to-use Company Profile. This website was built using the Next.js framework.",
       image: "https://github.com/KevinRTG/kevin-portofolio-website/blob/master/public/images/preview.png?raw=true",
-      link: "https://github.com/KevinRTG/CompanyProfile",
+      link: "https://solusidigital.my.id/",
+      technologies: ["Nodemailer", "React", "CSS", "JavaScript"],
     },
 
     {
@@ -15,6 +16,7 @@ function Projects() {
       description: "I created this congregation app project to be implemented in church organizations, which allows congregations to register through this web-based Jemaat App.",
       image: "https://github.com/KevinRTG/kevin-portofolio-website/blob/master/public/images/jemaatapppreview.png?raw=true",
       link: "https://sistem-pengelola-data-jemaat.vercel.app/",
+      technologies: ["Nodemailer", "React", "CSS", "JavaScript"],
     },
 
 
@@ -38,7 +40,16 @@ function Projects() {
             <div className="project-details">
               <h3 className="project-title">{project.title}</h3>
               <p className="project-description">{project.description}</p>
+              <div className="project-technologies">
+                {project.technologies.map((tech, i) => (
+                  <span key={i} className="tech-badge">
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
               <a href={project.link} className="project-link">
+
                 View Projects
               </a>
             </div>
